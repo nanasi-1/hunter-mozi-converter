c4f15af593b06b15571c057ef8ecbd1dd1c2df55
nanasi-1 <test-email>
2025-03-23 23:50:33 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="text-center selection:bg-green-900">
+      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
+      </header>
+    </div>
+  );
+};
+
+export default App;
